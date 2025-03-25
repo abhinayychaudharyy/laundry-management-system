5433bab51d41835624f7887e4b5eabff9ae97bc7
Aviral <aviral.srivastava.dev@gmail.com>
2025-03-25 19:09:58 +0530

    initial

---
+++ src/main/java/com/spinx/spinx/SpinxApplication.java
@@ -0,0 +1,13 @@
+package com.spinx.spinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spinx/spinx/SpinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spinx.spinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
